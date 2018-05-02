410b521b7401f73ce40355555a970a8e14005d01
Vitor buxbaum <vitorbuxbaum@gmail.com>
2018-05-02 01:51:27 -0300

    TP finalizado. Termina com Timeout do recv()

--- dcc-net.py
+++ dcc-net.py
@@ -2,6 +2,7 @@
 
 import socket
 import sys
+import time
 import struct
 import codecs
 import threading
@@ -9,6 +10,9 @@ import binascii
 
 # -*- coding: ascii -*-
 
+
+###############################################################################	
+###############################################################################	
 my_id = '\x01'
 
 def id_generator():
@@ -19,31 +23,43 @@ def id_generator():
 		my_id = '\x01'
 	return my_id
 
+
+###############################################################################	
 ###############################################################################	
 def bytes_to_int(b):
     return int(binascii.hexlify(''.join(i for i in b).encode('latin-1')), 16)
 
+
+###############################################################################	
 ###############################################################################	
 def list_to_str(b):
     return ''.join(i for i in b)
 
+
+###############################################################################
 ###############################################################################
 def decode16 (b_input):
 	#print("#debug in DECODE: ", b_input)
 	data = codecs.decode(b_input, 'hex')
 	return list(data.decode("latin-1"))
 
+
+###############################################################################
 ###############################################################################
 def encode16 (b_input):
 	#print("#debug in ENCODE: ", b_input)
 	data = ''.join(str(i) for i in b_input)
 	return codecs.encode(data.encode('latin-1'),'hex')    
 
+
+###############################################################################
 ###############################################################################
 def carry_around_add(a, b):
 	c = a + b
 	return(c &0xFFFF)+(c >>16)
 
+
+###############################################################################
 ###############################################################################
 def checksum(msg):
 	if(len(msg)%2 == 1):
@@ -55,6 +71,8 @@ def checksum(msg):
 		s = carry_around_add(s, w)
 	return ~s & 0xFFFF
 
+
+###############################################################################
 ###############################################################################
 def create_frame(data,length,ID,flag):
 	sync = "\xdc\xc0\x23\xc2"
@@ -80,73 +98,29 @@ def create_frame(data,length,ID,flag):
 
 	return frame
 
-###############################################################################
-# Returns the beginning of a new frame. -1 if sync not found
-def find_sync(msg):
-	length = len(msg)
-	for i in range(0,length - 4):	
-		sync = msg[i:i+4]
-		j = i + 4
-		if(msg[j:j+4] == sync):
-			return i
-	return -1
 
 ###############################################################################
-def get_data_from_frame(frame):
-	return frame[14:]
+###############################################################################
+# Checks if is sync pattern
+def is_double_sync(msg):
+	return msg == ['Ü', 'À', '#', 'Â', 'Ü', 'À', '#', 'Â']
+
 
 ###############################################################################
-def receive_frame(msg):
-	try:
-		#Finds the beginning of a frame, in case there was some transmission problem 
-		frame_begin = find_sync(msg)
-		if(frame_begin == -1):
-			print("ERROR RECEIVING FRAME") #Didn't find the beginning of the frame
-			return -1
-		frame = msg[frame_begin:] # Frame transmitted
-		orig_chksum = frame[10:12] #received checksum
-		# Get infos of the frame
-		sync1 = frame[0:4]
-		sync2 = frame[4:8]
-		length = frame[8:10]
-		chksum = '\x00\x00'
-		ID = frame[12]
-		flag = frame[13]
-		data = frame[14:14+bytes_to_int(length)]
-		if(''.join(i for i in length) == '\x00\x00' and flag == '\x80'): # Frame received is a ACK
-			#needs to check if ID is the same as the one saved by the system
-			print("ACK RECEIVED")
-			return 1
-		else: # Frame received is a regular frame
-			frame = []
-			
-			#Frame without checksum
-			for field in [sync1,sync2,length,chksum,ID,flag]:
-				for c in field:
-					frame.append(c)
-			for c in data:
-				frame.append(c)
-
-			if (struct.pack('!H', checksum(frame)).decode('latin-1') == ''.join(i for i in orig_chksum)):
-				return 1
-			else:
-				print("ERROR RECEIVING FRAME 2")
-				return -1
-	except:
-		print("ERROR RECEIVING FRAME 3")
+###############################################################################
+def check_ack(frame):
+	
+	ID = frame[12]
+	flag = frame[13]
+	
+	if(flag == '\x80'): # Frame received is a ACK
+		#print("ACK RECEIVED")
+		return 1 if ID == '\x01' else 0 #returns value for ID set in ACK
+	else: # Frame received is a regular frame
 		return -1
 
-###############################################################################
-def work(sckt):
-	str_size 	= struct.unpack(">i",sckt.recv(4))[0] 	# Recebe tamanho da string
-	msg 		= sckt.recv(str_size).decode()			# Recebe string
-	X 			= struct.unpack(">i",sckt.recv(4))[0] 	# Recebe chave para cifra de cesar
-	#print (msg)
-	msg  = decripto(msg,X)
-	print (msg)
-	sckt.send(str.encode(msg))
-	sckt.close()
 
+###############################################################################
 ###############################################################################
 def start_node():
 	#   args [1 = -s|-c , 2 = [<IP server>:]port, 3 = input file, 4 = output file ]
@@ -165,23 +139,28 @@ def start_node():
 		HOST = arg_2[0]
 		PORT = int(arg_2[1])
 
-		# dest = (HOST, PORT)
-		# tcp.connect(dest)
-		# sckt = tcp
+		dest = (HOST, PORT)
+		tcp.connect(dest)
+		sckt = tcp
 	elif (args[1] == "-s"):	# CALLED AS SERVER 
 		PORT = int(arg_2[0])
 
-		# tcp.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		# time_out = struct.pack("2l", *[15,0])
-		# tcp.setsockopt( socket.SOL_SOCKET, socket.SO_RCVTIMEO, time_out)
-		# orig = (HOST, PORT)
-		# tcp.bind(orig)
-		# tcp.listen(1)
-		# sckt,client = tcp.accept()
+		tcp.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+		time_out = struct.pack("2l", *[5,0])
+		tcp.setsockopt( socket.SOL_SOCKET, socket.SO_RCVTIMEO, time_out)
+
+		orig = (HOST, PORT)
+
+		tcp.bind(orig)
+		tcp.listen(1)
+		sckt,client = tcp.accept()
+	else:
+		print ("MUST DECLARE IF IT IS CLIENT OR SERVER")
+		return -1
 	# open files for input and output
 	file_IN = open(args[3],"rb")
-	file_OUT = open(args[4],"wb")
+	
 
 	#read input
 	n_packs = 0
@@ -189,40 +168,86 @@ def start_node():
 	b_in = file_IN.read(1)
 	data_set = [[]]
 	while (b_in):
-		data_set[n_packs].append(b_in.decode())
+		data_set[n_packs].append(b_in.decode('latin-1'))
 		count_len += 1
 		b_in = file_IN.read(1)
 		if count_len == 65535:
 			count_len = 0
 			n_packs += 1
 			data_set.append([])
-			
+	file_IN.close()	
+
 	#create the protocol's frames
+	frame_set = []
 	for data in data_set:
-		frame = create_frame(data,struct.pack('!H', len(data)),id_generator(),'\x00')	
+		frame = create_frame(data,struct.pack('!H', len(data)),id_generator(),'\x7f')	
 
 		#encode the frame
-		frame = encode16(frame)
-
-		######(Send frame)########
-
-	############################################################
-		######(Receive frame)#######
-
-
-		#dcc023c2dcc023c2........
-		msg = decode16(frame)
-		#if (receive_frame(list('\xdc\xc0\x23\xc2\xdc\xc0\x23\xc2\x00\x00\x00\x00\x00\x80')) == 1):  Test ACK
-		if (receive_frame(msg) == 1):
-			data = get_data_from_frame(msg)
-			#write data
-			#send ack
+		frame_set.append((1 if my_id == '\x01' else 0,encode16(frame)))
+		print (data)
 		
 		print ("DECODED DATA: \n", list_to_str(data), "\n")
 
 
+	file_OUT = open(args[4],"wb")
+	f_index = 0 
+	f_id = ''
+	while (1):
+		if f_index >= 0:
+			print("PACK SENT", f_index)
+			sckt.send(frame_set[f_index][1])
 
-	file_IN.close()
+		sckt.settimeout(15)
+		f_sync = decode16(sckt.recv(8))
+		if f_sync == []:
+			break
+		while 1: # procura por par de SYNCs 
+			print ("hmm", f_sync)
+			sync2 = decode16(sckt.recv(8))
+			f_sync.extend(sync2)
+			if is_double_sync(f_sync):
+				print ("ok")
+				break
+			else:
+				print ("ops", f_sync)
+				f_sync = sync2
+			
+		f_header = decode16(sckt.recv(12)) # recebe resto do header
+		f_sync.extend(f_header); f_header = f_sync # agrupa sync sync e header -> header completo
+		print (args[1], f_header) # printa header completo
+		d_length = bytes_to_int(f_header[8:10])
+
+		if (d_length == 0): #recebeu um frame sem dados (provavel ACK)
+			print("ACK RECEBIDO >>> ", f_index )
+			if  (f_index >= 0) and (check_ack(f_header) == frame_set[f_index][0]): #se recebeu um ACK esperado
+				print("ACK CONFIRMA >>> ", frame_set[f_index][0])
+				if  f_index == len(frame_set) - 1: #confere se foi o ultimo frame a ser enviado
+					f_index = -1
+				else:
+					f_index += 1 
+					print("index ++")
+			else:
+				time.sleep(1)
+			
+		elif (d_length > 0) : # receberá dados de um frame
+			f_data = decode16(sckt.recv(d_length*2)) # recebe os dados do frame
+			ACK_to_send = create_frame([],struct.pack('!H', 0),f_header[12], '\x80') # prepara ACK a ser enviado
+			f_header.extend(f_data); f_complete = f_header # agrupa header e dados -> f_complete
+			if struct.pack('!H', checksum(f_complete)).decode('latin-1') == '\x00\x00':
+				print("ACK SENT")
+				sckt.send(encode16(ACK_to_send))
+				if f_id != f_header[12]:
+					f_id = f_header[12]
+					for b in f_data:
+						file_OUT.write(b.encode('latin-1'))
+		
+
+
+			
+	#end while		
+
+	sckt.close()
+	
 	file_OUT.close()
 
 
